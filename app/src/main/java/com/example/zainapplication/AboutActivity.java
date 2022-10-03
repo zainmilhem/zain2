e75debc1ee81af31694a8e0b56975cf7c97e8fd0
zainmilhem <milhemzain123@gmail.com>
2022-10-03 10:05:22 +0300

    qq

---
+++ app/src/main/java/com/example/zainapplication/AboutActivity.java
@@ -0,0 +1,4 @@
+package com.example.zainapplication;
+
+public class AboutActivity {
+}
---
+++ app/src/main/java/com/example/zainapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.zainapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editEmail,editPassword;
+    Button buttonLogin,buttonsignup;
+    TextView textViewresult;
+
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editEmail = findViewById(R.id.editTextTextEmailAddress);
+        editPassword = findViewById(R.id.editTextTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonsignup = findViewById(R.id.buttonsignup);
+
+        sharedPreferences = getSharedPreferences("user_info",0);
+    }
+    public void login(View view){
+        //this line gets the registered email and password, in case no user was registered empty string is returned
+        String email = sharedPreferences.getString("email","");
+        String password = sharedPreferences.getString("password","");
+
+        String etEmail = editEmail.getText().toString();
+        String etPass = editPassword.getText().toString();
+
+        if (email.equals(etEmail) && password.equals(etPass)){
+            Intent i = new Intent(this, AboutActivity.class);
+            startActivity(i);
+        }else{
+            Toast.makeText(this, "Error! wrong email or password", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void signup(View view){
+        String etEmail = editEmail.getText().toString();
+        String etPass = editPassword.getText().toString();
+        //saves the email value in the prefrences and commits the file
+        sharedPreferences.edit().putString("email",etEmail).commit();
+        sharedPreferences.edit().putString("pasword",etPass).commit();
+
+    }
+}
